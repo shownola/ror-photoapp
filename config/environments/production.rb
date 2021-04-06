6276d2cfbfc177638523d49cd7fcbd83380777cd
shownola <swasieko@gmail.com>
2021-04-06 10:41:12 -0700

    Add sending email in production functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,11 +10,15 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => 'shownola-photo-app.herokuapp.com', :protocol => 'https'}
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # Ensures that a  master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
