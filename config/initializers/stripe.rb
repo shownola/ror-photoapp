755a2ceedc36f3b7d7d3f3f024b80ec834139e3b
Sherry Wasieko <swasieko@gmail.com>
2021-04-08 06:37:19 -0700

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,24 +5,3 @@ Rails.configuration.stripe = {
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-# Rails.configuration.stripe = {
-#   :publishable_key => ENV[:STRIPE_TEST_PUBLISHABLE_KEY],
-#   :secret_key => ENV[:STRIPE_TEST_SECRET_KEY]
-# }
-#
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
-#
-#
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
-#
-
-# Rails.configuration.stripe = {
-#   :publishable_key => ENV['pk_test_JlSHGGGYWkOhvVn10YRhRfxc'],
-#   :secret_key      => ENV['sk_test_szSE5c7vwU2wNu2CsWXmQeD5']
-# }
-# Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-
-# STRIPE_TEST_PUBLISHABLE_KEY='pk_test_JlSHGGGYWkOhvVn10YRhRfxc'
-# STRIPE_TEST_SECRET_KEY='sk_test_szSE5c7vwU2wNu2CsWXmQeD5'
