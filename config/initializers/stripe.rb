32bcf148bc59c56f577da0247729acf84fe15282
shownola <swasieko@gmail.com>
2021-04-08 13:58:15 -0700

    Fix stripe api credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,5 +3,9 @@ Rails.configuration.stripe = {
   :secret_key => ENV['SECRET_KEY']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
 
+#Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+# PUBLISHABLE_KEY = Rails.application.credentials[:stripe][:publishable_key]
+# SECRET_KEY = Rails.application.credentials[:stripe][:secret_key]
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_04_08_123110) do
 
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
